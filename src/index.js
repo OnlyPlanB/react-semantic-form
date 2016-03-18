2b677300480cf42cd12db101f82e36eac9c35c99
Ranjan Shrestha <ranjan@sharingapples.com>
2016-03-18 17:51:14 +0545

    Fine tuned

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import Form from './Form';
+import Form, { Model } from './Form';
 export ValidationError from './ValidationError';
 export * from './inputs';
+export Input from './Input';
+
+const Fieldset = Form.Fieldset;
+export { Fieldset, Model };
 
 export default Form;
